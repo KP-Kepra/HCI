890909fdb206ea8e13fec8d59dbd50d4629a4837
KP-Kepra <kevinpr153@gmail.com>
2017-09-29 01:43:18 +0800

    Added Vue.JS only website

---
+++ kp-kepra-comp4461/src/components/Main.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <navbar></navbar>
+    <agile :arrows="false" :infinite="false">
+        <div class="slide slide--1"><home></home></div>
+        <div class="slide slide--2"><about></about></div>
+        <div class="slide slide--3"><timeline></timeline></div>
+        <div class="slide slide--4"><blog></blog></div>
+        <div class="slide slide--5"><contact></contact></div>
+    </agile>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueAgile from 'vue-agile'
+
+import home from './home/home.vue'
+import about from './about.vue'
+import timeline from './timeline.vue'
+import blog from './blog.vue'
+import contact from './contact.vue'
+
+import navbar from './navbar.vue'
+
+Vue.use(VueAgile)
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    navbar,
+    home,
+    about,
+    timeline,
+    blog,
+    contact
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ kp-kepra-comp4461/src/components/navbar.vue
@@ -0,0 +1,55 @@
+// navBar.vue
+
+
+// HTML
+<template>
+<nav class="navbar-fixed-bottom">
+	<ul class="nav-justified" width="100%">
+		<div 	v-for="button in buttons" 
+				v-on:click="greet" 
+				class="navbar-nav">
+			{{ button.name }}
+		</div>
+	</ul>
+</nav>
+</template>
+
+
+// JS
+<script>
+var buttons = 
+[{
+	name: 'HOME',
+},
+
+{
+	name: 'ABOUT',
+},
+
+{
+	name: 'WORK',
+},
+
+{
+	name: 'BLOG',
+},
+
+{
+	name: 'CONTACT',
+}];
+
+export default {
+	name: 'navbar',
+	data: function()
+	{
+		return { buttons };
+	},
+
+	methods: 
+	{
+		greet: function() {
+			alert('Hello');
+		}
+	}
+}
+</script>
